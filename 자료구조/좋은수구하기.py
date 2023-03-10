295df6143872c71d084f89e8174944dbf97fc557
Karla <karlako9482@gmail.com>
2023-03-10 11:22:47 +0900

    좋은 수 구하기 오류 수정

--- 자료구조/좋은수구하기.py
+++ 자료구조/좋은수구하기.py
@@ -7,23 +7,20 @@ cnt = 0
 # 다른 두 수의 합인 수의 개수
 for k in  range(n):
     val = list[k]
-    # 2포인터
+
     i = 0
     j = n-1
-
-    sum = list[i] + list[j]
-    print(sum)
-    while i < j:
-        if sum == val:
+    while i < j:  # 투 포인터 알고리즘
+        if list[i] + list[j] == val:
             # 자기 자신을 좋은 수 만들기에 포함하면 안됨
             if i != k and j != k:
-                cnt = cnt + 1
+                cnt += 1
                 break
             elif i == k:
                 i += 1
             elif j == k:
                 j -= 1
-        elif sum > val:
+        elif list[i] + list[j] > val:
             j -= 1
         else:
             i += 1
