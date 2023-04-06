663c51b0a1b45a3e6ab445933a3fe9f1cd5dfc89
Karla <karlako9482@gmail.com>
2023-04-06 15:51:40 +0900

    이모티콘할인행사(완전탐색, 조합 라이브러리, zip())

---
+++ kakao/이모티콘할인행사.py
@@ -0,0 +1,37 @@
+def solution(users, emoticons):
+
+    from itertools import product
+    answer = [0, 0]
+
+    # 모든 할인율 중복 조합
+    combi = product((40, 30, 20, 10), repeat=len(emoticons))
+    for discounts in combi:
+
+        sold = [0, 0]  # 이모티콘, 판매액
+
+        # 각 유저
+        for user_discount, user_money in users:
+            # print("user : ", user_discount, user_money)
+
+            sold_emoticons = 0
+
+            # 각 이모티콘과 할인율
+            for emoticon, discount in zip(emoticons, discounts):
+
+                # 사용자 할인율 기준보다 많이 할인하면 구매
+                if discount >= user_discount:
+                    sold_emoticons += emoticon * (1 - discount / 100)
+                    # print("sold_emoticons", int(sold_emoticons))
+
+
+            # 구매비용이 임티플러스 기준금액보다 크면 임티플러스 가입
+            if sold_emoticons >= user_money:
+                sold[0] += 1
+            else: # 아니면 그냥 구매
+                sold[1] += sold_emoticons
+
+        # print(answer)
+        answer = max(answer, sold)
+    return answer
+
+print(solution([[40, 10000], [25, 10000]], [7000, 9000] ))
