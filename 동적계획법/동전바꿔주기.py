3440fa1c48ebbf80a82d66371b5874d48a31493e
Karla <karlako9482@gmail.com>
2023-06-01 00:55:41 +0900

    동전 dp

--- 동적계획법/동전바꿔주기.py
+++ 동적계획법/동전바꿔주기.py
@@ -7,16 +7,17 @@
 """
 t=int(input()) # 지폐금액
 k=int(input()) # 동전가지수
-coin=[(0,0)]
+coins=[(0,0)]
 for i in range(k):
     pi,ni=map(int, input().split())
-    coin.append((pi,ni))
+    coins.append((pi,ni))
 
+# dp[i][j] : i번째 동전까지 사용해서 j원을 만들 수 있는 경우의 수
 dp = [[0 for _ in range(t+1)] for _ in range(k+1)]
-dp[0][0] = 1
+dp[0][0] = 1 # 0원은 아무 동전도 사용하지 않는 경우가 하나 있으므로 1로 초기화
 
 for i in range(1, k+1): # 각 동전마다
-    val, cnt = coin[i]
+    val, cnt = coins[i]
     for j in range(t+1): # T원까지
         dp[i][j] = dp[i-1][j]
         for v in range(1, cnt+1):# 동전 갯수만큼 반복
@@ -24,5 +25,4 @@ for i in range(1, k+1): # 각 동전마다
                 dp[i][j] += dp[i-1][j-v*val]
             else:
                 break
-
 print(dp[k][t])
