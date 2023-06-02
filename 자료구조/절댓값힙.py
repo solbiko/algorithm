a789f1176bb2faa07a05840816c2458d58ad4a17
Karla <karlako9482@gmail.com>
2023-06-02 18:01:29 +0900

    힙

---
+++ 자료구조/절댓값힙.py
@@ -0,0 +1,17 @@
+import sys
+input=sys.stdin.readline
+from heapq import heappush, heappop
+
+n=int(input())
+q = []
+
+for _ in range(n):
+    num=int(input())
+    if num==0:
+        if len(q) > 0:
+            print(heappop(q)[1])
+        else:
+            print(0)
+    else:
+        heappush(q, (abs(num), num))
+
---
+++ 자료구조/최소힙2.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+from heapq import heappush, heappop
+
+n=int(input())
+q = []
+
+for _ in range(n):
+    num=int(input())
+    if num==0:
+        if len(q)>0:
+            print(heappop(q))
+        else:
+            print(0)
+    else:
+        heappush(q, num)
