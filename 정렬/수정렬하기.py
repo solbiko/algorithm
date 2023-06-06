2a32cab74fb1a0ace1135956007654dfcb7c7d96
Karla <karlako9482@gmail.com>
2023-06-06 11:40:00 +0900

    수정

--- 정렬/수정렬하기.py
+++ 정렬/수정렬하기.py
@@ -1,20 +1,18 @@
-
 n = int(input()) #1000까지
 list = [0]*n
 
 for i in range(n):
     list[i] = int(input())
-
-print(list)
+# print(list)
 
 # 오름차순
 # 버블정렬 n^2
 for i in range(n):
     for j in range(n-i-1):
-        print(j)
         if list[j] > list[j+1]:
             temp = list[j]
             list[j] = list[j+1]
             list[j+1] = temp
-    i += 1
-print(list)
+
+for i in range(n):
+    print(list[i])
