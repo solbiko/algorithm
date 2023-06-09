9ba2f61dc7d77c98a530439822ba9c3a4a9756bd
Karla <karlako9482@gmail.com>
2023-06-09 09:53:25 +0900

    역원구하기 주석추가

--- 정수론/역원구하기.py
+++ 정수론/역원구하기.py
@@ -3,11 +3,15 @@
 Zn ∋ a, b, c 일 때,
 (a+b) mod n = 0이면 b는 a의 덧셈역이라고 하고
 (a*c) mod n = 1이면 c는 a의 곱셈역이라고 한다.
+
 정수 N, A가 주어졌을 때 Zn에서의 A의 덧셈역과 곱셈역을 구하시오.
 단, 곱셈역을 구할 수 없으면 -1을 출력한다.
 """
 
-N, A = map(int, input().split())
+n,a= map(int, input().split())
+
+addInv=n-a  # 덧셈역원
+mulInv=-1  # 곱셈역원
 
 def gcd(a, b):
     return gcd(b, a%b) if b else a
@@ -33,12 +37,12 @@ def xgcd(a, b):
         t1 = t2
         t2 = t
 
-
-if gcd(A, N) != 1:
-    inv = -1
+if gcd(a,n)!=1:  #  곱셈역을 구할 수 없는 경우 -1을 출력, a*s 와 n이 서로소가 아니라는 것
+    print(addInv, mulInv)
 else:
-    inv = xgcd(A, N)
-    while inv <= 0:
-        inv += N
+    # xgcd(11*x,26)=1이 되는 x를 찾는 것 -> 11*s+26*t=1일 때 s값(곱셈역)
+    mulInv=xgcd(a,n)
+    while mulInv<=0:  # s가 양수인 경우
+        mulInv+=n
 
-print(N-A, inv)
+print(addInv, mulInv)
