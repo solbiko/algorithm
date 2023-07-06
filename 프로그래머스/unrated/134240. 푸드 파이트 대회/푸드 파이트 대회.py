8ef8abd2241e22a3b6f7b4414ef1dd538890e881
Karla <karlako9482@gmail.com>
2023-07-06 10:04:14 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,7 @@
+def solution(food):
+    answer = ''
+    for i in range(len(food)):
+        for j in range(1,food[i]//2+1):
+            answer+=str(i)
+    answer+='0'+answer[-1::-1]
+    return answer
