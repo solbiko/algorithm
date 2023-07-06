7d21832d9d22419bbcbafe63737fbb202d07e77b
Karla <karlako9482@gmail.com>
2023-07-06 09:50:33 +0900

    [unrated] Title: 추억 점수, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -1,13 +1,3 @@
 def solution(name, yearning, photo):
     answer = []
-    
-    d = {i:j for i,j in zip(name,yearning)}
-    
-    for x in photo:
-        score=0
-        for i in x:
-            if i in d:
-                score+=d[i]
-        answer.append(score)
-    
     return answer
