d85740a1cd643767c96e6d5c05b37bb5449a2ea4
Karla <karlako9482@gmail.com>
2023-07-12 02:56:53 +0900

    [level 2] Title: 예상 대진표, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -1,13 +1,17 @@
 import math
 def solution(n,a,b):
-    answer = 0
-    
-    def search(x,y,cnt):
-        x2=x/2
-        y2=y/2
-        if x2==y2:
-            return cnt-1
-        else:
-            return search(math.ceil(x2),math.ceil(y2), cnt+1)
+#     def search(x,y,cnt):
+#         x2=x/2
+#         y2=y/2
+#         if x2==y2:
+#             return cnt-1
+#         else:
+#             return search(math.ceil(x2),math.ceil(y2), cnt+1)
 
-    return search(a,b,1)
+#     return search(a,b,1)
+
+    # a, b 를 xor 취하는 과정에서 ab 사이의 거리가 가까우면 상위비트는 차이가 나지않음
+    # 거꾸로 ab 사이의 거리가 멀면 상위비트가 차이남
+    return ((a-1)^(b-1)).bit_length()
+
+    
