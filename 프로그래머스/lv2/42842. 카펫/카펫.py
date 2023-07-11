9d68be87ba6cbfc4d62bd7d8e9a4d0b8ab37f0f3
Karla <karlako9482@gmail.com>
2023-07-12 01:54:50 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -3,7 +3,5 @@ def solution(brown, yellow):
     for i in range(1,s//2):
         if s%i==0:
             if i<=s//i and i>2:
-                h=i
-                w=s//i
-                if (w-2)*(h-2)==yellow:
-                    return [w,h]
+                if (s//i-2)*(i-2)==yellow:
+                    return [s//i,i]
