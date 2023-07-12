ae676140f08c271854fe67169d5b0a5eebdee030
Karla <karlako9482@gmail.com>
2023-07-12 17:03:44 +0900

    [level 3] Title: 스티커 모으기(2), Time: 64.30 ms, Memory: 17.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -0,0 +1,19 @@
+def solution(sticker):
+
+    if len(sticker) == 1:
+        return sticker[0]
+    
+    dp = [0 for _ in range(len(sticker))] 
+    dp[0] = sticker[0]
+    dp[1] = sticker[0]
+    for i in range(2, len(sticker) - 1):
+        dp[i] = max(dp[i-2] + sticker[i], dp[i-1])
+
+    
+    dp2 = [0 for _ in range(len(sticker))]
+    dp2[0] = 0
+    dp2[1] = sticker[1]
+    for i in range(2, len(sticker)):
+        dp2[i] = max(dp2[i-2] + sticker[i], dp2[i-1])
+
+    return max( max(dp), max(dp2))
