da6afd27d3f5e61b3a78f4d0c3854e46e37aad61
Karla <karlako9482@gmail.com>
2023-07-19 11:10:13 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 75.20 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -1,19 +1,25 @@
 def solution(arr):
     answer = [0, 0]
 
-    def quad(arr,r,c,n):
-        tg = arr[r][c]
+    def quad(r,c,n):
+        
+        if len(arr) == 1:
+            answer[arr[r][c]] += 1
+            return
+        else:
+            tg = arr[r][c]
+            
         for i in range(n):
             for j in range(n):
                 if arr[r+i][c+j] != tg:
                     n=n//2
-                    quad(arr, r, c, n)
-                    quad(arr, r, c+n, n)
-                    quad(arr, r+n, c, n)
-                    quad(arr, r+n, c+n, n)
+                    quad(r, c, n)
+                    quad(r, c+n, n)
+                    quad(r+n, c, n)
+                    quad(r+n, c+n, n)
                     return
         answer[tg] += 1
     
-    quad(arr, 0, 0, len(arr))
+    quad(0, 0, len(arr))
     
     return answer
