0141e8fe44df78d12e13f10715c47752b3b80ba4
Karla <karlako9482@gmail.com>
2023-07-21 09:47:32 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -1,22 +1,30 @@
 from collections import deque
 def solution(begin, target, words):
     answer = 0
+    
+    v=[0]*len(words) # 단어 사용체크 리스트
+    
     q = deque()
     q.append([begin, 0])
-    V = [ 0 for i in range(len(words))]
+    
     while q:
         word, cnt = q.popleft()
+        
         if word == target:
             answer = cnt
             break        
+        
         for i in range(len(words)):
+            word2=words[i]
             temp_cnt = 0
-            if not V[i]:
-                for j in range(len(word)):
-                    if word[j] != words[i][j]:
+            
+            if not v[i]: # 사용안한경우
+                # 단어 두개 비교
+                for j in range(len(word)): 
+                    if word[j] != word2[j]:
                         temp_cnt += 1
                 if temp_cnt == 1:
-                    q.append([words[i], cnt+1])
-                    V[i] = 1
+                    q.append([word2, cnt+1])
+                    v[i] = 1 # 사용처리
                     
     return answer
