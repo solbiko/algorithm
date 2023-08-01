ecd27853a1d0608021380c68bb6587559a8900d5
Karla <karlako9482@gmail.com>
2023-08-01 14:08:35 +0900

    [unrated] Title: 무인도 여행, Time: 0.43 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
@@ -0,0 +1,54 @@
+import copy
+from collections import deque
+dr=[0,1,0,-1]
+dc=[1,0,-1,0]
+
+def solution(maps):
+    answer=[]
+    
+    n,m=len(maps), len(maps[0])
+    visited= [[False]*m for i in range(n)] # BFS 시 방문 여부 저장 리스트
+
+    def BFS(i,j):
+        cnt=0
+
+        queue=deque()
+        queue.append([i,j])
+        cnt+=int(maps[i][j])
+        visited[i][j]=True
+
+        while queue:
+            r,c=queue.popleft()
+            for d in range(4):
+                tempR=dr[d]
+                tempC=dc[d]
+                nextR= r+tempR
+                nextC= c+tempC
+                while 0<=nextR<n and 0<=nextC<m:
+                    if not visited[nextR][nextC] and maps[nextR][nextC]!='X':
+                        visited[nextR][nextC]=True # 방문 표시
+                        cnt+=int(maps[nextR][nextC]) # 식량 수 카운트
+                        queue.append([nextR,nextC])
+                    else:
+                        break
+                    if tempR<0:
+                        tempR-=1
+                    elif tempR>0:
+                        tempR+=1
+                    elif tempC<0:
+                        tempC-=1
+                    elif tempC>0:
+                        tempC+=1
+        return cnt
+
+    # 섬 구분
+    for i in range(n):
+        for j in range(m):
+            if maps[i][j] != 'X' and not visited[i][j]:
+                total_cnt = copy.deepcopy(BFS(i,j)) # 총 식량
+                answer.append(total_cnt)
+                
+    if answer:
+        return sorted(answer)
+    else:
+        return [-1]
