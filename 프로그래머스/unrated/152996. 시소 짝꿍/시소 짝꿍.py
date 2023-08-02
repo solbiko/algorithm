3a905e918be3e39ab0fe7f041a82426fc8725b70
Karla <karlako9482@gmail.com>
2023-08-02 21:59:07 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
@@ -2,11 +2,10 @@ from collections import Counter
 def solution(weights):
     answer = 0
     counter = Counter(weights)
-    for k,v in counter.items():
-        c=counter[k]
-        answer += c*(c-1)//2
-        answer += c*counter[k*3/2]
-        answer += c*counter[k*2]
-        answer += c*counter[k*4/3]
+    for c in counter:
+        answer += counter[c]*(counter[c]-1)//2
+        answer += counter[c]*counter[c*3/2]
+        answer += counter[c]*counter[c*2]
+        answer += counter[c]*counter[c*4/3]
 
     return answer
