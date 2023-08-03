297e44dcaebfa2471cd54d2722b2abce2ea85f19
Karla <karlako9482@gmail.com>
2023-08-03 17:48:55 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -1,14 +1,5 @@
 def solution(s):
-    answer = 0
-    
-    for i in range(len(s)):
-        for j in range(len(s), i, -1):
-            if j - i < answer: 
-                break
-                
-            x=s[i:j]
-            if x == x[::-1]:
-                answer=j-i
-                break
-            
-    return answer
+    for i in range(len(s),0,-1): # 문자열 길이 긴 것 부터
+        for j in range(len(s)-i+1): # 문자열 시작 인덱스
+            if s[j:j+i] == s[j:j+i][::-1]:  
+                return i
