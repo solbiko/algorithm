0066f908db2c7876aac3d4b5896636f8f794f6ad
Karla <karlako9482@gmail.com>
2023-08-05 00:38:25 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.23 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
+++ 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -1,24 +1,13 @@
 def solution(cards):
-    answer=0
+    answer = []
     
-    group=[]
-    visited=[False]*(len(cards)+1)
-    
-    for x in cards:
-        if not visited[x]:
-            temp=[]
-            while True:
-                if x in temp:
-                    break
-                temp.append(x)
-                visited[x]=True
-                x=cards[x-1]
-                
-            group.append(len(temp))
-    
-    if len(group)>1:
-        group.sort(reverse=True)
-        answer = group[0]*group[1]
-    
-    return answer
-    
+    for x in range(len(cards)):
+        temp = []
+        while cards[x] not in temp:
+            temp.append(cards[x])
+            x = cards[x]-1
+            
+        answer.append([] if sorted(temp) in answer else sorted(temp))
+    answer.sort(key=len)
+
+    return len(answer[-1]) * len(answer[-2])
