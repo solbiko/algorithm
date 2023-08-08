362022c6ab14a2203f9dfd70a26f5553dd0ba367
Karla <karlako9482@gmail.com>
2023-08-08 11:44:19 +0900

    [unrated] Title: 연속 펄스 부분 수열의 합, Time: 210.15 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/unrated/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/unrated/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -1,21 +1,8 @@
-# 연속 펄스 부분 수열의 합 중 가장 큰 것
 def solution(sequence):
-    n=len(sequence)
-    
-    temp1=[sequence[i]*(-1)**(i+1) for i in range(n)] # -1 1 펄스 곱한 리스트
-    temp2=[sequence[i]*(-1)**i for i in range(n)] # 1 -1 펄스 곱한 리스트
-    
-    dp1=[0]*n
-    dp1[0]=temp1[0]
-    
-    dp2=[0]*n
-    dp2[0]=temp2[0]
 
-    for i in range(1, n):
-        dp1[i] = max(dp1[i-1]+temp1[i], temp1[i])
-        dp2[i] = max(dp2[i-1]+temp2[i], temp2[i])
+    answer = 0
+    sumlist = [0]
+    for i in range(len(sequence)):
+        sumlist.append(sumlist[-1]+(-1)**i*sequence[i])
     
-    return max(max(dp1), max(dp2))
-
-
-    
+    return abs(max(sumlist) - min(sumlist))
