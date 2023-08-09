e3d74d0bbb6b2dac4b2df60a1f58a32d59f268d1
Karla <karlako9482@gmail.com>
2023-08-10 08:42:23 +0900

    [level 4] Title: 도둑질, Time: 584.40 ms, Memory: 92.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/42897. 도둑질/도둑질.py
@@ -0,0 +1,20 @@
+def solution(money):
+    answer = 0
+    
+    dp = [0 for _ in range(len(money))] 
+    dp[0] = money[0]
+    dp[1] = money[0]
+    for i in range(2, len(money) - 1):
+        dp[i] = max(dp[i-2] + money[i], dp[i-1])
+
+
+    dp2 = [0 for _ in range(len(money))]
+    dp2[0] = 0
+    dp2[1] = money[1]
+    for i in range(2, len(money)):
+        dp2[i] = max(dp2[i-2] + money[i], dp2[i-1])
+
+    return max(max(dp), max(dp2))
+
+    
+    return answer
