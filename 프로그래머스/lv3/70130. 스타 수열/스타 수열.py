3049a75d9d824351f6cddec6a17d056ed3a39689
Karla <karlako9482@gmail.com>
2023-08-10 10:10:17 +0900

    [level 3] Title: 스타 수열, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/70130. 스타 수열/스타 수열.py
@@ -0,0 +1,25 @@
+from collections import Counter
+def solution(a):
+    answer = 0
+
+    if len(a) <= 1:
+        return 0
+
+    c=Counter(a)
+    
+    for i in c:
+        if c[i]*2 <= answer: # i의 등장 횟수가 공통인자 횟수 이하
+            continue
+        cnt = 0
+        idx = 0
+        while idx < len(a)-1:
+            if (a[idx]!=i and a[idx+1]!=i) or a[idx] == a[idx+1]:
+                # 집합에 두 값 모두 i가 아님, 집합에 두 값이 같음 -> 스타수열 X
+                idx += 1
+                continue
+            cnt+=2
+            idx+=2
+
+        answer = max(answer, cnt)
+
+    return answer
