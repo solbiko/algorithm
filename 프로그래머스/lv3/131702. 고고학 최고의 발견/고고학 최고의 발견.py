d6463737d6d6d0af94c4a3c2da77f9d6c21ab2e1
Karla <karlako9482@gmail.com>
2023-08-22 16:43:29 +0900

    [level 3] Title: 고고학 최고의 발견, Time: 5830.30 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/131702. 고고학 최고의 발견/고고학 최고의 발견.py
+++ 프로그래머스/lv3/131702. 고고학 최고의 발견/고고학 최고의 발견.py
@@ -1,35 +1,31 @@
 from itertools import product
-dx=[0,0,0,1,-1]
-dy=[0,1,-1,0,0]
+dr = [0, 0, 0, 1, -1]
+dc = [0, 1, -1, 0, 0]
 
 def solution(clockHands):
-    answer=int(1e9)
-    
-    n=len(clockHands)
-    
-    def calc(x,y):
-        temp=0
-        for d in range(5):
-            nx,ny = x+dx[d], y+dy[d]
-            if 0<=nx<n and 0<=ny<n:
-                temp+=data[nx][ny]
-        return (clockHands[x][y]+temp)%4
+    answer = int(1e9)
+    n = len(clockHands)
 
-    
+    def calc(r, c):
+        temp = 0
+        for d in range(5):
+            nr, nc = r + dr[d], c + dc[d]
+            if 0 <= nr < n and 0 <= nc < n:
+                temp += data[nr][nc]
+        return (clockHands[r][c] + temp) % 4
+
     for li in product(range(4), repeat=n):
-        data=[[0]*n for _ in range(n)]
-        data[0]=list(li).copy() # 첫째행
-        
-        for i in range(1,n):
+        data = [[0] * n for _ in range(n)]
+
+        data[0] = list(li)  # 1행
+        for i in range(1, n):  # 2~(n-1)행
             for j in range(n):
-                data[i][j]=(4-calc(i-1,j))%4
-                
+                data[i][j] = (4 - calc(i - 1, j)) % 4
+
         for i in range(n):
-            if calc(n-1, i) !=0:
+            if calc(n-1, i) != 0:
                 break
         else:
-            temp=sum([sum(t) for t in data])
-            answer= min(answer, temp)
-                
+            answer = min(answer, sum([sum(t) for t in data]))
 
     return answer
