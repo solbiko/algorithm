0429ee6c9e55fd4735a0203b5c481f1319f15059
Karla <karlako9482@gmail.com>
2023-08-28 17:05:33 +0900

    [unrated] Title: 아방가르드 타일링, Time: 58.00 ms, Memory: 13.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181186. 아방가르드 타일링/아방가르드 타일링.py
+++ 프로그래머스/unrated/181186. 아방가르드 타일링/아방가르드 타일링.py
@@ -1,55 +1,17 @@
-import sys
-sys.setrecursionlimit(10**6)
-dp=[0]*100001
-
 def solution(n):
-    
-    def block(n):
-        answer = 0
- 
-        if n==1:
-            dp[1]=1
-            return 1
-        if n==2:
-            dp[2]=3
-            return 3
-        if n==3:
-            dp[3]=10
-            return 10
-        if n==4:
-            dp[4]=23
-            return 23
-        if n==5:
-            dp[5]=62
-            return 62
-        if n==6:
-            dp[6]=170
-            return 170
-        
-        if dp[n-1]:
-            answer+=dp[n-1]
-        else:
-            answer += block(n-1)
-        if dp[n-2]:
-            answer += dp[n-2]*2
-        else:
-            answer += block(n-2)*2
-        if dp[n-3]:
-            answer += dp[n-3]*6
-        else:
-            answer += block(n-3)*6
-        if dp[n-4]:
-            answer += dp[n-4]*1
-        else:
-            answer += block(n-4)*1
-        if dp[n-6]:
-            answer -= dp[n-6]*1
-        else:
-            answer -= block(n-6)*1
-        
-        dp[n]=answer
-        return answer
 
-    return block(n) % 1000000007
+    dp=[0]*100001
     
-  
+    dp[1]=1
+    dp[2]=3
+    dp[3]=10
+    dp[4]=23
+    dp[5]=62
+    dp[6]=170
+    
+    if n>6:
+        for i in range(7,n+1):
+            dp[i] = (dp[i-1] + dp[i-2]*2 + dp[i-3]*6 + dp[i-4] - dp[i-6]) % 1000000007
+
+    return dp[n]
+    
